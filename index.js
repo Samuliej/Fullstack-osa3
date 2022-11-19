b10a257406c97f9107c7387e5229928d09aea1cc
Samuli Toppi <samuli.e.j.toppi@student.jyu.fi>
2022-11-19 13:20:33 +0200

    3.3: puhelinluettelon backend step3 complete

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const app = express()
 
 let persons = [
     {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456"
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
     },
     {
-      id: 2,
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
     },
     {
-      id: 3,
-      name: "Dan Abramov",
-      number: "12-43-234345"
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
     },
     {
         id: 4,
@@ -33,6 +33,13 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(per => per.id === id)
+    if (person) response.json(person)
+    else response.status(404).end()
 })
 
 const PORT = 3001
