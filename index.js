b83597e7d917185c4943c237535735c7c36175c8
Samuli Toppi <samuli.e.j.toppi@student.jyu.fi>
2022-11-28 17:00:11 +0200

    3.13 & 3.14: puhelinluettelo ja tietokanta step1 & step2 complete (backend)

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(express.json())
 
-let persons = [
+/*let persons = [
     {
         id: 1,
         name: "Arto Hellas",
@@ -26,7 +28,7 @@ let persons = [
         name: "Mary Poppendick",
         number: "39-23-6423122"
     }
-  ]
+  ] */
 
 app.use(cors())
 app.use(morgan(':method :url :res[content-length] - :response-time ms :postContent'))
@@ -37,40 +39,45 @@ morgan.token('postContent', function(req, res) {
     return JSON.stringify(requestContent);
 })
 
-app.get('/info', (request, response) => {
+/*app.get('/info', (request, response) => {
     const date = new Date()
     response.send('<p>Phonebook has info for ' + persons.length + ' people</p>' +
                  '<p>' + date + '</p>')
-})
+}) */
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(per => per.id === id)
-    if (person) response.json(person)
-    else response.status(404).end()
+    Person.findById(request.params.id).then(note => {
+        response.json(person)
+    })
 })
 
 app.post('/api/persons', (request, response) => {
-    const person = request.body
-    const duplicateName = persons.find(per => per.name === person.name)
+    const personInfo = request.body
+    //const duplicateName = persons.find(per => per.name === personInfo.name)
 
-    if (!person.name || !person.number) {
+    if (!personInfo.name || !personInfo.number) {
         response.status(400).json({
             error: "Name or number must not be empty."
         })
-    } else if (duplicateName) {
+    } /*else if (duplicateName) {
         response.status(400).json({
             error: "Name must be unique"
+        }) */
+     else {
+        const person = new Person({
+            name: personInfo.name,
+            number: personInfo.number,
         })
-    } else {
-        person.id = Math.floor(Math.random() * 100000)
-        persons = persons.concat(person)
         console.log(person)
-        response.json(person)
+        person.save().then(savedPerson => {
+            response.json(savedPerson)
+        })
     }
 })
 
@@ -80,7 +87,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = process.env.PORT || "3001"
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
